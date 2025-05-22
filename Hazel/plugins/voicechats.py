8cd4d7aa94a574c268db8fd8181048e67a326011
Otazuki004 <otazuki004@gmail.com>
2025-05-22 19:32:13 +0000

    Update. (i miss her tbh.)

--- Hazel/plugins/voicechats.py
+++ Hazel/plugins/voicechats.py
@@ -2,8 +2,14 @@ from .. import *
 from pyrogram import *
 from MultiSessionManagement import *
 
-@on_message(filters.command('joinvc', prefixes=HANDLER)&filters.me)
+@on_message(filters.command('joinvc', prefixes=HANDLER)&filters.me&filters.group)
 async def joinvc(c,m):
   pytgcalls_client = clients_data[c.me.id]["pytgcalls_client"]
   await pytgcalls_client.play(m.chat.id)
   await m.reply("Okay, i've joined in vc.")
+  
+@on_message(filters.command('leavevc', prefixes=HANDLER)&filters.me&filters.group)
+async def leavevc(c,m):
+  pytgcalls_client = clients_data[c.me.id]["pytgcalls_client"]
+  await pytgcalls_client.leave_call(m.chat.id)
+  await m.reply("left from the vc.")  
